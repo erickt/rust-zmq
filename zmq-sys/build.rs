dbbe506fdd727846060b598807f4c46a83946fda
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-02-11 17:18:57 -0500

    Maybe it'll build without libsodium?

--- zmq-sys/build.rs
+++ zmq-sys/build.rs
@@ -2,7 +2,6 @@ extern crate "pkg-config" as pkg_config;
 
 fn main() {
     pkg_config::find_library("libzmq").unwrap();
-    pkg_config::find_library("libsodium").unwrap();
+    // pkg_config::find_library("libsodium").unwrap();
     println!("cargo:rustc-flags=-l stdc++");
-    // pkg_config::find_library("libstdc++").unwrap();
 }
