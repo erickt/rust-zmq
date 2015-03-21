783c25a1fe17270f86e86595ba90d850d2007ab5
Frank Denis <github@pureftpd.org>
2015-03-21 09:38:15 +0100

    Use thread::sleep() in order to finally get rid of old_io

--- examples/zguide/helloworld-server/main.rs
+++ examples/zguide/helloworld-server/main.rs
@@ -1,5 +1,5 @@
 #![crate_name = "helloworld-server"]
-#![feature(old_io, std_misc)]
+#![feature(std_misc, thread_sleep)]
 
 /// Hello World server in Rust
 /// Binds REP socket to tcp://*:5555
@@ -7,7 +7,7 @@
 
 extern crate zmq;
 
-use std::old_io;
+use std::thread;
 use std::time::Duration;
 
 fn main() {
@@ -21,6 +21,6 @@ fn main() {
         responder.recv(&mut msg, 0).unwrap();
         println!("Received {}", msg.as_str().unwrap());
         responder.send_str("World", 0).unwrap();
-        old_io::timer::sleep(Duration::seconds(1));
+        thread::sleep(Duration::seconds(1));
     }
 }
