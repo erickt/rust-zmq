80b68a31be467c3b2c0def8274ab1f36a213e506
Deyan Ginev <d.ginev@jacobs-university.de>
2016-08-29 13:06:37 +0300

    updating to new std::time interface

--- examples/zguide/fileio3/main.rs
+++ examples/zguide/fileio3/main.rs
@@ -1,25 +1,25 @@
 #![crate_name = "fileio3"]
 
 //  File Transfer model #3
-//  
+//
 //  In which the client requests each chunk individually, using
 //  command pipelining to give us a credit-based flow control.
 
 extern crate zmq;
-extern crate time;
 extern crate tempfile;
 extern crate rand;
 
-use zmq::{SNDMORE};
+use zmq::SNDMORE;
 use std::thread;
-use std::io::{Seek,SeekFrom,Write,Read,Error};
-use rand::{random,Rng};
-use tempfile::TempFile;
+use std::io::{Seek, SeekFrom, Write, Read, Error};
+use rand::Rng;
+use tempfile::tempfile;
+use std::fs::File;
 
-pub static CHUNK_SIZE : usize = 250000;
-pub static CHUNK_SIZE_STR : &'static str = "250000";
-pub static PIPELINE : usize = 10;
-pub static PIPELINE_HWM : usize = 20;
+pub static CHUNK_SIZE: usize = 250000;
+pub static CHUNK_SIZE_STR: &'static str = "250000";
+pub static PIPELINE: usize = 10;
+pub static PIPELINE_HWM: usize = 20;
 
 fn random_string(length: usize) -> String {
   let mut string = String::new();
@@ -30,25 +30,25 @@ fn random_string(length: usize) -> String {
   string
 }
 
-fn client_thread(expected_total : usize) {
+fn client_thread(expected_total: usize) {
   let mut context = zmq::Context::new();
   let mut dealer = context.socket(zmq::DEALER).unwrap();
-  let identity : String = (0..10).map(|_| rand::random::<u8>() as char).collect();
+  let identity: String = (0..10).map(|_| rand::random::<u8>() as char).collect();
   dealer.set_identity(identity.as_bytes()).unwrap();
 
   assert!(dealer.connect("tcp://localhost:6000").is_ok());
 
-  //  Up to this many chunks in transit
+  // Up to this many chunks in transit
   let mut credit = PIPELINE;
-  
+
   let mut total = 0;       //  Total bytes received
   let mut chunks = 0;      //  Total chunks received
   let mut offset = 0;      //  Offset of next chunk request
-  
+
   let mut clean_break = false;
   loop {
     while (credit > 0) && !clean_break {
-      //  Ask for next chunk
+      // Ask for next chunk
       dealer.send_str("fetch", SNDMORE).unwrap();
       dealer.send_str(&offset.to_string(), SNDMORE).unwrap();
       dealer.send_str(CHUNK_SIZE_STR, 0).unwrap();
@@ -85,61 +85,61 @@ fn client_thread(expected_total : usize) {
 //  The server thread waits for a chunk request from a client,
 //  reads that chunk and sends it back to the client:
 
-fn server_thread (file : &mut TempFile) -> Result<(),Error> {
+fn server_thread(file: &mut File) -> Result<(), Error> {
 
-    let mut context = zmq::Context::new();
-    let mut router = context.socket(zmq::ROUTER).unwrap();
-    //  We have two parts per message so HWM is PIPELINE * 2
-    router.set_sndhwm(PIPELINE_HWM as i32).unwrap();
-    assert!(router.bind("tcp://*:6000").is_ok());
+  let mut context = zmq::Context::new();
+  let mut router = context.socket(zmq::ROUTER).unwrap();
+  // We have two parts per message so HWM is PIPELINE * 2
+  router.set_sndhwm(PIPELINE_HWM as i32).unwrap();
+  assert!(router.bind("tcp://*:6000").is_ok());
 
-    loop {
-      //  First frame in each message is the sender identity
-      let mut identity = zmq::Message::new().unwrap();
-      router.recv(&mut identity, 0).unwrap();
-      if identity.as_str().unwrap().is_empty() {
-        break;              //  Shutting down, quit
-      }
-          
-      //  Second frame is "fetch" command
-      let mut command_msg = zmq::Message::new().unwrap();
-      router.recv(&mut command_msg, 0).unwrap();
-      let command = command_msg.as_str().unwrap();
-      assert!(command == "fetch");
-
-      //  Third frame is chunk offset in file
-      let mut offset_msg = zmq::Message::new().unwrap();
-      router.recv(&mut offset_msg, 0).unwrap();
-      let offset = offset_msg.as_str().unwrap().parse::<u64>().unwrap();;
-
-      //  Fourth frame is maximum chunk size
-      let mut chunk_size_msg = zmq::Message::new().unwrap();
-      router.recv(&mut chunk_size_msg, 0).unwrap();
-      let chunk_size = chunk_size_msg.as_str().unwrap().parse::<usize>().unwrap();;
-            
-      // Seek to offset
-      file.seek(SeekFrom::Start(offset)).unwrap();
-      //  Read chunk of data from file
-      let mut data = vec![0; chunk_size];
-      let size = try!(file.read(&mut data));
-      data.truncate(size);
-      //  Send resulting chunk to client
-      router.send_msg(identity, SNDMORE).unwrap();
-      router.send(&data,0).unwrap();
+  loop {
+    // First frame in each message is the sender identity
+    let mut identity = zmq::Message::new().unwrap();
+    router.recv(&mut identity, 0).unwrap();
+    if identity.as_str().unwrap().is_empty() {
+      break;              //  Shutting down, quit
     }
-    Ok(())
+
+    // Second frame is "fetch" command
+    let mut command_msg = zmq::Message::new().unwrap();
+    router.recv(&mut command_msg, 0).unwrap();
+    let command = command_msg.as_str().unwrap();
+    assert!(command == "fetch");
+
+    // Third frame is chunk offset in file
+    let mut offset_msg = zmq::Message::new().unwrap();
+    router.recv(&mut offset_msg, 0).unwrap();
+    let offset = offset_msg.as_str().unwrap().parse::<u64>().unwrap();;
+
+    // Fourth frame is maximum chunk size
+    let mut chunk_size_msg = zmq::Message::new().unwrap();
+    router.recv(&mut chunk_size_msg, 0).unwrap();
+    let chunk_size = chunk_size_msg.as_str().unwrap().parse::<usize>().unwrap();;
+
+    // Seek to offset
+    file.seek(SeekFrom::Start(offset)).unwrap();
+    // Read chunk of data from file
+    let mut data = vec![0; chunk_size];
+    let size = try!(file.read(&mut data));
+    data.truncate(size);
+    // Send resulting chunk to client
+    router.send_msg(identity, SNDMORE).unwrap();
+    router.send(&data, 0).unwrap();
+  }
+  Ok(())
 }
 
 //  The main task starts the client and server threads; it's easier
 //  to test this as a single process with threads, than as multiple
 //  processes:
 fn main() {
-  //  Start child threads
+  // Start child threads
   thread::spawn(|| {
 
     // Generate test data in a temp directory
     println!("Generating temporary data...");
-    let mut file = TempFile::new().unwrap();
+    let mut file = tempfile().unwrap();
     // Prepare some random test data of appropriate size
     file.write(random_string(10 * CHUNK_SIZE).as_bytes()).unwrap();
 
@@ -151,6 +151,6 @@ fn main() {
     // Start client thread
     client_thread(10 * CHUNK_SIZE);
   });
-  //  Loop until client tells us it's done
+  // Loop until client tells us it's done
   client_child.join().unwrap();
 }
--- examples/zguide/rtdealer/main.rs
+++ examples/zguide/rtdealer/main.rs
@@ -3,47 +3,46 @@
 ///  Router-to-dealer example
 
 extern crate zmq;
-extern crate time;
 extern crate rand;
 
-use zmq::{SNDMORE};
-use rand::{random,Rng};
-use time::{Duration,PreciseTime};
+use zmq::SNDMORE;
+use rand::Rng;
+use std::time::{Duration, Instant};
 use std::thread;
 
 fn worker_task() {
   let mut context = zmq::Context::new();
   let mut worker = context.socket(zmq::DEALER).unwrap();
   let mut rng = rand::thread_rng();
-  let identity : String = (0..10).map(|_| rand::random::<u8>() as char).collect();
+  let identity: String = (0..10).map(|_| rand::random::<u8>() as char).collect();
   worker.set_identity(identity.as_bytes()).unwrap();
   assert!(worker.connect("tcp://localhost:5671").is_ok());
   let mut total = 0;
   loop {
     // Tell the broker we're ready for work
     worker.send(b"", SNDMORE).unwrap();
-    worker.send_str("Hi boss!",0).unwrap();
+    worker.send_str("Hi boss!", 0).unwrap();
     // Get workload from broker, until finished
     let mut envelope = zmq::Message::new().unwrap();
     let mut workload = zmq::Message::new().unwrap();
     worker.recv(&mut envelope, 0).unwrap(); // envelope delimiter
     worker.recv(&mut workload, 0).unwrap();
-    let work =  workload.as_str().unwrap();
+    let work = workload.as_str().unwrap();
     if work == "Fired!" {
-      println!("Worker {} completed {} tasks",identity, total);
+      println!("Worker {} completed {} tasks", identity, total);
       break;
     }
     total += 1;
 
     // Do some random work
-    thread::sleep_ms(rng.gen_range(1, 500));
+    thread::sleep(Duration::from_millis(rng.gen_range(1, 500)));
   }
 }
 
 
 fn main() {
   let worker_pool_size = 10;
-  let allowed_duration = Duration::seconds(5);
+  let allowed_duration = Duration::new(5, 0);
   let mut context = zmq::Context::new();
   let mut broker = context.socket(zmq::ROUTER).unwrap();
   assert!(broker.bind("tcp://*:5671").is_ok());
@@ -60,14 +59,14 @@ fn main() {
   }
 
   // Run for five seconds and then tell workers to end
-  let start_time = PreciseTime::now();
+  let start_time = Instant::now();
   let mut workers_fired = 0;
   loop {
     // Next message gives us least recently used worker
     let mut identity = zmq::Message::new().unwrap();
     broker.recv(&mut identity, 0).unwrap();
     broker.send_msg(identity, SNDMORE).unwrap();
-    
+
     let mut envelope = zmq::Message::new().unwrap();
     let mut workload = zmq::Message::new().unwrap();
     broker.recv(&mut envelope, 0).unwrap(); // Envelope
@@ -75,10 +74,10 @@ fn main() {
     broker.send(b"", SNDMORE).unwrap();
 
     // Encourage workers until it's time to fire them
-    if start_time.to(PreciseTime::now()) < allowed_duration {
-      broker.send_str("Work harder",0).unwrap();
+    if start_time.elapsed() < allowed_duration {
+      broker.send_str("Work harder", 0).unwrap();
     } else {
-      broker.send_str("Fired!",0).unwrap();
+      broker.send_str("Fired!", 0).unwrap();
       workers_fired += 1;
       if workers_fired >= worker_pool_size {
         break;
