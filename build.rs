830530c5245024556f504ec44b5e7102fd2bae6e
Will <tcosprojects@gmail.com>
2016-10-06 06:48:29 -0700

    Make zmq_has optional in build script

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
 
+#[cfg(feature = "zmq_has")]
 fn main() {
 	for has in ["ipc", "pgm", "tipc", "norm", "curve", "gssapi"].into_iter() {
 		if unsafe { zmq_has(CString::new(has.as_bytes()).unwrap().as_ptr()) } == 1 {
@@ -9,6 +10,12 @@ fn main() {
 	}
 }
 
+#[cfg(not(feature = "zmq_has"))]
+fn main() {
+	
+}
+
+#[cfg(feature = "zmq_has")]
 #[link(name = "zmq")]
 extern "C" {
 	fn zmq_has(capability: *const c_char) -> c_int;
