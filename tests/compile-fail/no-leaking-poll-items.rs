770f4b45e59f314300a76976b85680af38ed834a
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-11-12 14:40:03 -0800

    Fix compiletest failure on nightly

--- tests/compile-fail/no-leaking-poll-items.rs
+++ tests/compile-fail/no-leaking-poll-items.rs
@@ -4,6 +4,6 @@ fn main() {
     let mut context = zmq::Context::new();
     let _poll_item = {
         let socket = context.socket(zmq::PAIR).unwrap();
-        socket.as_poll_item(0) //~ ERROR `socket` does not live long enough
-    };
+        socket.as_poll_item(0)
+    }; //~ ERROR `socket` does not live long enough
 }
