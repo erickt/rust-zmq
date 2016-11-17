133e7e3fd5acd13781545e05c7483ec066832eed
Deyan Ginev <d.ginev@jacobs-university.de>
2016-11-17 16:51:45 +0100

    Add rtdealer and fileio3 examples
    This is a slightly tweaked squash merge of PR #77 (commits
    fdffb1..80b68a, see below) by @dginev. The tweaks were only:
    
    - Get rid of compiler warnings due to reduced mutability requirements of
      the binding API.
    
    - Remove a few superfluous "pub" keywords in the fileio3 example.
    
    - Fixup identity output in worker, and switch identities to random raw
      byte slices, instead of using random sequences of unicode codepoints
      in the 0-255 range encoded as UTF-8.
    
    > commit 80b68a31be467c3b2c0def8274ab1f36a213e506
    > Author: Deyan Ginev <d.ginev@jacobs-university.de>
    > Date:   Mon Aug 29 13:06:37 2016 +0300
    >
    >     updating to new std::time interface
    >
    > commit 19258a538feef09f5fdbda29227b611a17177b82
    > Author: Deyan Ginev <d.ginev@jacobs-university.de>
    > Date:   Mon Aug 24 18:04:31 2015 +0300
    >
    >     adding fileio3 example
    >
    > commit fdffb1e4e3c3632eea4cd93e499a4e0072951bd7
    > Author: Deyan Ginev <d.ginev@jacobs-university.de>
    > Date:   Sun Aug 23 21:40:32 2015 +0300
    >
    >     added rtdealer example

---
+++ examples/zguide/rtdealer/main.rs
@@ -0,0 +1,92 @@
+#![crate_name = "rtdealer"]
+
+///  Router-to-dealer example
+
+extern crate zmq;
+extern crate rand;
+
+use zmq::SNDMORE;
+use rand::Rng;
+use std::time::{Duration, Instant};
+use std::thread;
+
+// Inefficient but terse base16 encoder
+fn hex(bytes: &[u8]) -> String {
+    bytes.iter().map(|x| format!("{:02x}", x)).collect::<Vec<_>>().join("")
+}
+
+fn worker_task() {
+  let context = zmq::Context::new();
+  let mut worker = context.socket(zmq::DEALER).unwrap();
+  let mut rng = rand::thread_rng();
+  let identity: Vec<_> = (0..10).map(|_| rand::random::<u8>()).collect();
+  worker.set_identity(&identity).unwrap();
+  assert!(worker.connect("tcp://localhost:5671").is_ok());
+  let mut total = 0;
+  loop {
+    // Tell the broker we're ready for work
+    worker.send(b"", SNDMORE).unwrap();
+    worker.send_str("Hi boss!", 0).unwrap();
+    // Get workload from broker, until finished
+    let mut envelope = zmq::Message::new().unwrap();
+    let mut workload = zmq::Message::new().unwrap();
+    worker.recv(&mut envelope, 0).unwrap(); // envelope delimiter
+    worker.recv(&mut workload, 0).unwrap();
+    let work = workload.as_str().unwrap();
+    if work == "Fired!" {
+      println!("Worker {} completed {} tasks", hex(&identity), total);
+      break;
+    }
+    total += 1;
+
+    // Do some random work
+    thread::sleep(Duration::from_millis(rng.gen_range(1, 500)));
+  }
+}
+
+
+fn main() {
+  let worker_pool_size = 10;
+  let allowed_duration = Duration::new(5, 0);
+  let context = zmq::Context::new();
+  let mut broker = context.socket(zmq::ROUTER).unwrap();
+  assert!(broker.bind("tcp://*:5671").is_ok());
+
+  // While this example runs in a single process, that is just to make
+  // it easier to start and stop the example. Each thread has its own
+  // context and conceptually acts as a separate process.
+  let mut thread_pool = Vec::new();
+  for _ in 0..worker_pool_size {
+    let child = thread::spawn(move || {
+      worker_task();
+    });
+    thread_pool.push(child);
+  }
+
+  // Run for five seconds and then tell workers to end
+  let start_time = Instant::now();
+  let mut workers_fired = 0;
+  loop {
+    // Next message gives us least recently used worker
+    let mut identity = zmq::Message::new().unwrap();
+    broker.recv(&mut identity, 0).unwrap();
+    broker.send_msg(identity, SNDMORE).unwrap();
+
+    let mut envelope = zmq::Message::new().unwrap();
+    let mut workload = zmq::Message::new().unwrap();
+    broker.recv(&mut envelope, 0).unwrap(); // Envelope
+    broker.recv(&mut workload, 0).unwrap(); // Response from worker
+    broker.send(b"", SNDMORE).unwrap();
+
+    // Encourage workers until it's time to fire them
+    if start_time.elapsed() < allowed_duration {
+      broker.send_str("Work harder", 0).unwrap();
+    } else {
+      broker.send_str("Fired!", 0).unwrap();
+      workers_fired += 1;
+      if workers_fired >= worker_pool_size {
+        break;
+      }
+    }
+  }
+}
