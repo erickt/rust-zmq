54bec178c18f88952d25f02dff7468648dcbdfc2
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-12-08 09:26:27 -1000

    Get appveyor to compile zeromq
    This gets the appveyor build to work, and now it's failing due to #94.

--- zmq-sys/build.rs
+++ zmq-sys/build.rs
@@ -3,13 +3,32 @@ extern crate pkg_config;
 use std::env;
 
 fn main() {
-    if let Some(prefix) = env::var("LIBZMQ_PREFIX").ok() {
-        println!("cargo:rustc-link-search=native={}/lib", prefix);
-        println!("cargo:include={}/include", prefix);
-    } else {
-        match pkg_config::find_library("libzmq") {
-            Ok(pkg) => println!("{:?}", pkg),
-            Err(e) => panic!("Unable to locate libzmq, err={:?}", e),
+    let lib_path = env::var("LIBZMQ_LIB_DIR").ok().or_else(|| {
+        env::var("LIBZMQ_PREFIX").ok()
+            .map(|prefix| format!("{}/lib", prefix))
+    });
+
+    let include = env::var("LIBZMQ_INCLUDE_DIR").ok().or_else(|| {
+        env::var("LIBZMQ_PREFIX").ok()
+            .map(|prefix| format!("{}/include", prefix))
+    });
+
+    match (lib_path, include) {
+        (Some(lib_path), Some(include)) => {
+            println!("cargo:rustc-link-search=native={}", lib_path);
+            println!("cargo:include={}", include);
+        }
+        (Some(_), None) => {
+            panic!("Unable to locate libzmq include directory.")
+        }
+        (None, Some(_)) => {
+            panic!("Unable to locate libzmq library directory.")
+        }
+        (None, None) => {
+            match pkg_config::probe_library("libzmq") {
+                Ok(pkg) => println!("{:?}", pkg),
+                Err(e) => panic!("Unable to locate libzmq, err={:?}", e),
+            }
         }
     }
 }
