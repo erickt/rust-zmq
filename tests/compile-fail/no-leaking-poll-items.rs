4e93e97a1285b195239b7c711178c962b261db16
Andreas Rottmann <a.rottmann@gmx.at>
2016-12-21 22:30:09 +0100

    Introduce bitflags type for polling events
    This improves type safety, and allows code to written in a more
    readable style, at least for those not well-versed in C bitmaskery.
    
    On the flip side, this is another incompatible API change.

--- tests/compile-fail/no-leaking-poll-items.rs
+++ tests/compile-fail/no-leaking-poll-items.rs
@@ -4,6 +4,6 @@ fn main() {
     let context = zmq::Context::new();
     let _poll_item = {
         let socket = context.socket(zmq::PAIR).unwrap();
-        socket.as_poll_item(0)
+        socket.as_poll_item(zmq::POLLIN)
     }; //~ ERROR `socket` does not live long enough
 }
