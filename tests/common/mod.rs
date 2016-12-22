26a6c8953f44ea8b8dc92d3bf0789e62331f11a0
Andreas Rottmann <a.rottmann@gmx.at>
2016-12-22 22:15:04 +0100

    Split off `test!` macro into `common` module
    This allows us to re-use `test!` in `connection.rs` formerly known as
    `shared-context.rs`. The name change reflects a more general purpose
    of that file; some slight refactoring was done in light of that.
    
    In addition to adding a timeout to prevent hanging tests, `test!` now
    also initializes `env_logger`, which is a new dev-dependency.

---
+++ tests/common/mod.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+
+pub extern crate timebomb;
+extern crate env_logger;
+
+use std::sync::{Once, ONCE_INIT};
+
+static LOGGER_INIT: Once = ONCE_INIT;
+
+#[macro_export]
+macro_rules! test {
+    ($name:ident, $block:block) => {
+        #[test]
+        fn $name() {
+            $crate::common::ensure_env_logger_initialized();
+            $crate::common::timebomb::timeout_ms(|| {
+                $block
+            }, 10000);
+        }
+    }
+}
+
+pub fn ensure_env_logger_initialized() {
+    LOGGER_INIT.call_once(|| env_logger::init().unwrap());
+}
