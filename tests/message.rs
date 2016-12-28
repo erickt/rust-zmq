c3f40702a86b0e8134a400741c5f037f78da5ae2
Andreas Rottmann <a.rottmann@gmx.at>
2016-12-28 17:19:31 +0100

    Add `Eq` implementation for `Message`
    Including a few simple quickcheck tests.

---
+++ tests/message.rs
@@ -0,0 +1,31 @@
+extern crate zmq;
+#[macro_use]
+extern crate quickcheck;
+
+#[macro_use]
+mod common;
+
+use zmq::Message;
+use quickcheck::{Gen, Arbitrary};
+
+// A pair which contains two non-equal values
+#[derive(Clone, Debug)]
+struct NePair<T>(T, T);
+
+impl<T> Arbitrary for NePair<T> where T: Eq + Arbitrary {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        let v1 = T::arbitrary(g);
+        let v2 = (0..).map(|_| T::arbitrary(g)).filter(|v| *v != v1).next().unwrap();
+        NePair(v1, v2)
+    }
+}
+
+quickcheck! {
+    fn msg_cmp_eq(input: Vec<u8>) -> bool {
+        Message::from_slice(&input) == Message::from_slice(&input)
+    }
+
+    fn msg_cmp_ne(input: NePair<Vec<u8>>) -> bool {
+        Message::from_slice(&input.0) != Message::from_slice(&input.1)
+    }
+}
