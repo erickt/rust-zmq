6168f0a5d8903dab9a5299a8e037d851122cdfa3
Andreas Rottmann <a.rottmann@gmx.at>
2017-01-01 18:35:02 +0100

    Fix compiletests
    - Reduce error message to match against, as the exact error message
      issued by rustc changes from time to time.
    
    - Don't depend on `send` API changes in master in 0.8 branch.

--- tests/compile-fail/socket-thread-unsafe.rs
+++ tests/compile-fail/socket-thread-unsafe.rs
@@ -12,9 +12,9 @@ fn main() {
     let mut context = zmq::Context::new();
     let socket = t!(context.socket(zmq::REP));
     let s = &socket;
-    let t = thread::spawn(move || {  //~ ERROR he trait bound `*mut std::os::raw::c_void: std::marker::Sync` is not satisfied [E0277]
+    let t = thread::spawn(move || {  //~ ERROR trait bound
         t!(s.bind("tcp://127.0.0.1:12345"))
     });
-    socket.send(b"ABC", 0);
+    socket.send_str("ABC", 0);
     t.join().unwrap();
 }
