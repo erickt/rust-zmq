a2bb8b6a595bc8e8f1007610bfcaa966623114ef
Sergej Pupykin <sergej.pupykin@dsr-company.com>
2017-06-23 19:58:30 +0300

    build libzmq-pw statically from cargo

--- build.rs
+++ build.rs
@@ -2,10 +2,9 @@ extern crate zmq_pw_sys as zmq;
 
 fn main() {
     use std::ffi::CString;
-
-	for has in ["ipc", "pgm", "tipc", "norm", "curve", "gssapi"].into_iter() {
-		if unsafe { zmq::zmq_has(CString::new(has.as_bytes()).unwrap().as_ptr()) } == 1 {
-			println!("cargo:rustc-cfg=ZMQ_HAS_{}=\"1\"", has.to_uppercase());
-		}
-	}
+    for has in ["ipc", "pgm", "tipc", "norm", "curve", "gssapi"].into_iter() {
+        if unsafe { zmq::zmq_has(CString::new(has.as_bytes()).unwrap().as_ptr()) } == 1 {
+            println!("cargo:rustc-cfg=ZMQ_HAS_{}=\"1\"", has.to_uppercase());
+        }
+    }
 }
--- zmq-pw-sys/build.rs
+++ zmq-pw-sys/build.rs
@@ -1,35 +1,116 @@
-extern crate metadeps;
+extern crate pkg_config;
 
+use std::fs;
+use std::process::{Command, Stdio};
 use std::env;
 use std::path::Path;
 
-fn prefix_dir(env_name: &str, dir: &str) -> Option<String> {
-    env::var(env_name).ok().or_else(|| {
-        env::var("LIBZMQ_PREFIX").ok()
-            .map(|prefix| Path::new(&prefix).join(dir))
-            .and_then(|path| path.to_str().map(|p| p.to_owned()))
-    })
-}
-
 fn main() {
-    let lib_path = prefix_dir("LIBZMQ_LIB_DIR", "lib");
-    let include = prefix_dir("LIBZMQ_INCLUDE_DIR", "include");
+    let cargo_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let output_dir = env::var("OUT_DIR").unwrap();
 
-    match (lib_path, include) {
-        (Some(lib_path), Some(include)) => {
-            println!("cargo:rustc-link-search=native={}", lib_path);
-            println!("cargo:include={}", include);
-        }
-        (Some(_), None) => {
-            panic!("Unable to locate libzmq include directory.")
-        }
-        (None, Some(_)) => {
-            panic!("Unable to locate libzmq library directory.")
-        }
-        (None, None) => {
-            if let Err(e) = metadeps::probe() {
-                panic!("Unable to locate libzmq:\n{}", e);
+    let src = Path::new(&cargo_dir[..]);
+    let dst = Path::new(&output_dir[..]);
+
+    let target = env::var("TARGET").unwrap();
+    println!("target={}", target);
+    match target.find("-windows-") {
+        Some(..) => {
+            // do not build c-code on windows, use binaries
+            let prebuilt_dir = env::var("SOVRIN_PREBUILT_DEPS_DIR").unwrap();
+            println!("cargo:rustc-link-search=native={}", prebuilt_dir);
+            println!("cargo:rustc-flags=-L {}/lib \
+                  -l libzmq-pw \
+                  ", prebuilt_dir);
+            println!("cargo:include={}/include/zmq-pw", prebuilt_dir);
+            return;
+        },
+        None => {}
+    }
+
+    let libs = vec!["libzmq-pw"];
+
+    let mut found = true;
+    for l in libs {
+        println!("searching for {}", l);
+        match pkg_config::find_library(l) {
+            Ok(..) => {},
+            Err(..) => {
+                println!("pkg-config cannot find {}", l);
+                let mdata = fs::metadata(
+                    &format!("{}/{}.a",
+                             dst.join("pkg/lib").to_str().unwrap(),
+                             l));
+                match mdata {
+                    Ok(md) => {
+                        if !md.is_file() {
+                            found = false;
+                            println!("not found");
+                            break;
+                        }
+                    },
+                    Err(..) => {
+                        found = false;
+                        println!("error getting metadata");
+                        break;
+                    }
+                }
             }
         }
     }
+
+    if found {
+        println!("cargo:rustc-flags=-L {}/lib \
+                  -l zmq-pw \
+                  ", dst.join("pkg").display());
+        println!("cargo:root={}", dst.join("pkg").display());
+        println!("cargo:include={}/include/zmq-pw", dst.join("pkg").display());
+        return;
+    }
+
+    let root = src.join("libzmq-pw");
+
+    let _ = fs::remove_dir_all(&dst.join("pkg"));
+    let _ = fs::remove_dir_all(&dst.join("build"));
+    fs::create_dir(&dst.join("build")).unwrap();
+
+    run(Command::new("sh")
+        .arg("-c")
+        .arg(&format!("cd {} && \
+                       cmake \
+                       -DCMAKE_BUILD_TYPE=Release \
+                       -DCMAKE_INSTALL_PREFIX=/ \
+                       -DCMAKE_INSTALL_LIBDIR=\"/lib\" \
+                       -DCMAKE_INSTALL_INCLUDEDIR=\"/include\" \
+                       -DBUILD_SHARED=OFF \
+                       -DBUILD_SHARED=ON \
+                       -DCMAKE_POSITION_INDEPENDENT_CODE=ON {}",
+                      dst.join("build").to_str().unwrap(),
+                      root.as_path().to_str().unwrap()))
+        .current_dir(&dst.join("build")));
+
+    run(Command::new("make")
+        .arg(&format!("-j{}", env::var("NUM_JOBS").unwrap()))
+        .current_dir(&dst.join("build")));
+
+    run(Command::new("make")
+        .arg(&format!("-j{}", env::var("NUM_JOBS").unwrap()))
+        .arg("install")
+        .arg(&format!("DESTDIR={}", dst.join("pkg").to_str().unwrap()))
+        .current_dir(&dst.join("build")));
+
+    println!("cargo:rustc-flags=-L {}/lib \
+              -l zmq-pw \
+              ", dst.join("pkg").display());
+    println!("cargo:root={}", dst.join("pkg").display());
+    println!("cargo:include={}/include/zmq-pw", dst.join("pkg").display());
+}
+
+fn run(cmd: &mut Command) {
+    println!("running: {:?}", cmd);
+    assert!(cmd.stdout(Stdio::inherit())
+               .stderr(Stdio::inherit())
+               .status()
+               .unwrap()
+               .success());
 }
