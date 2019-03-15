5b3daa9405e5adedbec0349d2e1b389e662cd4e5
Jonah Petri <jonah@petri.us>
2019-03-15 07:51:09 -0400

    add runtime fences for gssapi tests

--- tests/test.rs
+++ tests/test.rs
@@ -541,34 +541,42 @@ test!(test_disconnect_err, {
 });
 
 test!(test_getset_gssapi_server, {
-    let ctx = Context::new();
-    let sock = ctx.socket(REQ).unwrap();
-    sock.set_gssapi_server(true).unwrap();
-    assert_eq!(sock.is_gssapi_server().unwrap(), true);
+    if has("gssapi").unwrap_or(false) {
+        let ctx = Context::new();
+        let sock = ctx.socket(REQ).unwrap();
+        sock.set_gssapi_server(true).unwrap();
+        assert_eq!(sock.is_gssapi_server().unwrap(), true);
+    }
 });
 
 test!(test_getset_gssapi_principal, {
-    let ctx = Context::new();
-    let sock = ctx.socket(REQ).unwrap();
-    sock.set_gssapi_principal("principal").unwrap();
-    assert_eq!(sock.get_gssapi_principal().unwrap().unwrap(), "principal");
+    if has("gssapi").unwrap_or(false) {
+        let ctx = Context::new();
+        let sock = ctx.socket(REQ).unwrap();
+        sock.set_gssapi_principal("principal").unwrap();
+        assert_eq!(sock.get_gssapi_principal().unwrap().unwrap(), "principal");
+    }
 });
 
 test!(test_getset_gssapi_service_principal, {
-    let ctx = Context::new();
-    let sock = ctx.socket(REQ).unwrap();
-    sock.set_gssapi_service_principal("principal").unwrap();
-    assert_eq!(
-        sock.get_gssapi_service_principal().unwrap().unwrap(),
-        "principal"
-    );
+    if has("gssapi").unwrap_or(false) {
+        let ctx = Context::new();
+        let sock = ctx.socket(REQ).unwrap();
+        sock.set_gssapi_service_principal("principal").unwrap();
+        assert_eq!(
+            sock.get_gssapi_service_principal().unwrap().unwrap(),
+            "principal"
+        );
+    }
 });
 
 test!(test_getset_gssapi_plaintext, {
-    let ctx = Context::new();
-    let sock = ctx.socket(REQ).unwrap();
-    sock.set_gssapi_plaintext(true).unwrap();
-    assert_eq!(sock.is_gssapi_plaintext().unwrap(), true);
+    if has("gssapi").unwrap_or(false) {
+        let ctx = Context::new();
+        let sock = ctx.socket(REQ).unwrap();
+        sock.set_gssapi_plaintext(true).unwrap();
+        assert_eq!(sock.is_gssapi_plaintext().unwrap(), true);
+    }
 });
 
 test!(test_getset_handshake_ivl, {
