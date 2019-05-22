68f7da658a2ae330accf0c83339e2aa2996b938a
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-05-22 18:04:22 -0400

    Removed the static feature flag
    Static linking for the vendored crate is now hardcoded.

--- zmq-sys/build/vendored.rs
+++ zmq-sys/build/vendored.rs
@@ -1,13 +1,8 @@
-use std::env;
-
 pub fn configure() {
-    let wants_static =
-        cfg!(feature = "static") || env::var("ZMQ_SYS_STATIC").unwrap_or_default() == "1";
-
     // Whether the current profile is in debug.
     let wants_debug = cfg!(debug_assertions);
 
     println!("cargo:rerun-if-changed=build.rs");
-    let artifacts = zeromq_src::Build::new().link_static(wants_static).build_debug(wants_debug).build();
+    let artifacts = zeromq_src::Build::new().link_static(true).build_debug(wants_debug).build();
     artifacts.print_cargo_metadata();
 }
