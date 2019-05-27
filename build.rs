55e28052e604d5a04ec3bfb871457b23064589f7
Andreas Rottmann <a.rottmann@gmx.at>
2019-05-27 23:59:18 +0200

    Disable build-time feature probing
    The feature-probing was introduced originally in commit
    d8cb8ee ("Binding update for ZMQ v4.1") in 2016, and was probably
    intended to cope with potentially missing C ABI, allowing for rust-zmq
    to work with a range of different libzmq versions, including ones that
    expose a subset of the C ABI that was supported by rust-zmq. Tying it
    to the `zmq_has` runtime probe was a mistake, I realize now, from the
    very outset.
    
    Thus, going forward, after resolving this issue, rust-zmq will expose
    only functionality supported by the C ABI of the minimum libzmq
    version it requires, and will never conditionally disable parts of its
    API for older C ABI versions.
    
    Fixes: #275

--- build.rs
+++ build.rs
@@ -1,16 +1,6 @@
-use std::env;
-
 fn main() {
-    use std::ffi::CString;
-
     println!(
         "cargo:rustc-env=BUILD_PROFILE={}",
-        env::var("PROFILE").unwrap()
+        std::env::var("PROFILE").unwrap()
     );
-
-    for has in ["ipc", "pgm", "tipc", "norm", "curve", "gssapi"].into_iter() {
-        if unsafe { zmq_sys::zmq_has(CString::new(has.as_bytes()).unwrap().as_ptr()) } == 1 {
-            println!("cargo:rustc-cfg=ZMQ_HAS_{}=\"1\"", has.to_uppercase());
-        }
-    }
 }
