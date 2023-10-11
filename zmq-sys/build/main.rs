7d941474152bafa40275bef613d83031de5a52c3
conanoc <conanoc@gmail.com>
2023-10-11 14:49:37 +0900

    Support libsodium

--- zmq-sys/build/main.rs
+++ zmq-sys/build/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 pub fn configure() {
     println!("cargo:rerun-if-changed=build/main.rs");
     println!("cargo:rerun-if-env-changed=PROFILE");
@@ -6,8 +8,19 @@ pub fn configure() {
     // relying on `tweetnacl`. However since this `tweetnacl` [has never been
     // audited nor is ready for production](https://github.com/zeromq/libzmq/issues/3006),
     // we link against `libsodium` to enable `ZMQ_CURVE`.
+    let maybe_libsodium = if cfg!(feature = "libsodium") {
+        let lib_dir = env::var("DEP_SODIUM_LIB")
+            .expect("build metadata `DEP_SODIUM_LIB` required");
+        let include_dir = env::var("DEP_SODIUM_INCLUDE")
+            .expect("build metadata `DEP_SODIUM_INCLUDE` required");
+
+        Some(zeromq_src::LibLocation::new(lib_dir, include_dir))
+    } else {
+        None
+    };
+
     zeromq_src::Build::new()
-        .with_libsodium(None)
+        .with_libsodium(maybe_libsodium)
         .build();
 }
 
