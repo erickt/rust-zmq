acbe4f196280dda3197875f79d0b6bcdea1b3f81
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2024-05-30 15:56:00 -0700

    patch

--- zmq-sys/build/main.rs
+++ zmq-sys/build/main.rs
@@ -1,13 +1,23 @@
+extern crate zeromq_src;
+
+use std::env;
+use zeromq_src::LibLocation;
+
 pub fn configure() {
     println!("cargo:rerun-if-changed=build/main.rs");
     println!("cargo:rerun-if-env-changed=PROFILE");
+
+    // get sodium lib and include paths from environment
+    let sodium_paths = env::var("DEP_SODIUM_LIB")
+        .and_then(|lib| env::var("DEP_SODIUM_INCLUDE").map(|inc| LibLocation::new(lib, inc)))
+        .ok();
 
     // Note that by default `libzmq` builds without `libsodium` by instead
     // relying on `tweetnacl`. However since this `tweetnacl` [has never been
     // audited nor is ready for production](https://github.com/zeromq/libzmq/issues/3006),
     // we link against `libsodium` to enable `ZMQ_CURVE`.
     zeromq_src::Build::new()
-        .with_libsodium(None)
+        .with_libsodium(sodium_paths)
         .build();
 }
 
